6cb851d761d53b29d807a170edea70df111ca370
ahmetcaliskan63 <ahmetabclife@gmail.com>
2025-09-28 01:40:30 +0300

    apk dosyasını 21.0 mb düşürdüm

--- android/app/src/main/java/com/klucampus/app/MainActivity.kt
+++ android/app/src/main/java/com/klucampus/app/MainActivity.kt
@@ -5,10 +5,7 @@ import android.os.Bundle
 
 import com.facebook.react.ReactActivity
 import com.facebook.react.ReactActivityDelegate
-import com.facebook.react.defaults.DefaultNewArchitectureEntryPoint.fabricEnabled
 import com.facebook.react.defaults.DefaultReactActivityDelegate
-
-import expo.modules.ReactActivityDelegateWrapper
 
 class MainActivity : ReactActivity() {
   override fun onCreate(savedInstanceState: Bundle?) {
@@ -30,14 +27,7 @@ class MainActivity : ReactActivity() {
    * which allows you to enable New Architecture with a single boolean flags [fabricEnabled]
    */
   override fun createReactActivityDelegate(): ReactActivityDelegate {
-    return ReactActivityDelegateWrapper(
-          this,
-          BuildConfig.IS_NEW_ARCHITECTURE_ENABLED,
-          object : DefaultReactActivityDelegate(
-              this,
-              mainComponentName,
-              fabricEnabled
-          ){})
+    return DefaultReactActivityDelegate(this, mainComponentName, false)
   }
 
   /**
